ebb21b12b2be8c16fc285132dac3db755ac39137
Prasad Kakad <prasadkakad118@gmail.com>
2026-03-20 20:35:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+	let table=document.getElementById("sampleTable");
+    let newRow=document.createElement("tr");
+	newRow.innerHTML="<td>New Cell1</td> <td>New Cell2</td>" 
+	table.insertBefore(newRow,table.firstChild);
 }
+
